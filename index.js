a06b6abf309950d524e6b53af2b32fe9fbc5587e
Akbar Aditama <akbaraditamasp@gmail.com>
2023-05-15 19:30:04 +0700

    initial

---
+++ index.js
@@ -0,0 +1,39 @@
+const { default: axios } = require("axios");
+
+module.exports = {
+  init(providerOptions) {
+    const client = axios.create({
+      baseURL: "https://www.idprocloud.com/api/v1",
+      headers: {
+        "PRO-Box-Token": providerOptions.boxToken,
+      },
+    });
+
+    return {
+      upload(file) {
+        const formData = new FormData();
+        formData.append("name", file.name);
+        formData.append("file", file.buffer, { filename: file.name });
+
+        return client.post("/box", formData).then((response) => {
+          file.hash = response.data.key;
+
+          return file;
+        });
+      },
+      delete(file) {
+        return client.delete("/box/" + file.hash).then(() => {
+          return;
+        });
+      },
+      getSignedUrl(file) {
+        return client.get("/box/" + file.hash).then((response) => {
+          return response.data;
+        });
+      },
+      isPrivate() {
+        return true;
+      },
+    };
+  },
+};
